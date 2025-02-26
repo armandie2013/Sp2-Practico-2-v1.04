bdc532c05926c7ece82c3f6c8c747fb859909a88
= <=>
2025-02-26 12:27:59 -0300

    Metodos eliminar y busar creados

--- app.js
+++ app.js
@@ -8,9 +8,8 @@ mongoose
   .then(() => console.log("Conexion exitosa con MongoDB"))
   .catch((error) => console.error("Error al conectar MongoDB"));
 
-
 // Crea un esquema y un modelo //
-  const superheroSchema = new mongoose.Schema(
+const superheroSchema = new mongoose.Schema(
   {
     nombreSuperHeroe: { type, String, required: true },
     nombreReal: { type, String, required: true },
@@ -28,25 +27,50 @@ mongoose
 
 const SuperHero = mongoose.model("SuperHero", superheroSchema);
 
-
 // Metodo CRUD para insertar, actualizar, eliminar y buscar //
-async function insertSuperHero(){
-    const hero=new SuperHero({
-        nombreSuperHeroe:"Spiderman",
-        nombreReal:"Peter Parker",
-        edad:25,
-        planetaOrigen:"Tierra",
-        debilidad:"Radioactiva",
-        poderes:['Trepar paredes', 'Sentido aracnido', 'Super fuerza', 'Agilidad'],
-        aliados: ["Ironman"],
-        enemigos:["Duende Verde"],
-        creador:"Diego"
-    });
-    await hero.save();
-    console.log("Superheroe insertado", hero);
+async function insertSuperHero() {
+  const hero = new SuperHero({
+    nombreSuperHeroe: "Spiderman",
+    nombreReal: "Peter Parker",
+    edad: 25,
+    planetaOrigen: "Tierra",
+    debilidad: "Radioactiva",
+    poderes: ["Trepar paredes", "Sentido aracnido", "Super fuerza", "Agilidad"],
+    aliados: ["Ironman"],
+    enemigos: ["Duende Verde"],
+    creador: "Diego",
+  });
+  await hero.save();
+  console.log("Superheroe insertado", hero);
 }
 
 insertSuperHero();
 
+// Metodo para actualizar //
+async function updateSuperHero(nombreSuperHeroe) {
+  const result = await SuperHero.updateOne(
+    { nombreSuperHeroe: nombreSuperHeroe },
+    { $set: { edad: 26 } }
+  );
+}
 
+updateSuperHero("Spiderman");
 
+// Metodo para eliminar //
+async function deleteSuperHero(nombreSuperHeroe) {
+  const result = await SuperHero.deleteOne({
+    nombreSuperHeroe: nombreSuperHeroe,
+  });
+  console.log("Superheroe eliminado:", result);
+}
+
+deleteSuperHero("Spiderman");
+
+// Metodo para buscar //
+
+async function findSuperHeroes() {
+  const heroes = await SuperHero.find({ planetaOrigen: "Tierra" });
+  console.log("Superheroes encontrados:", heroes);
+}
+
+findSuperHeroes();
